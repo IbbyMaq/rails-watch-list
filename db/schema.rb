0fde5b610c6d24ccee87a49cc5dcac16632e070a
Ibrahim Maqsood ninjaibmaq@gmail.com <ninjaibmaq@gmail.com>
2022-04-23 15:44:27 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_005309) do
+ActiveRecord::Schema.define(version: 2022_04_23_185617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2022_04_22_005309) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "overview"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
